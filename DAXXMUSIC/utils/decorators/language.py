d41761363025d4085e07f0b9ed963b4c681d9fbe
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-27 03:58:16 +0700

    Update language.py

--- DAXXMUSIC/utils/decorators/language.py
+++ DAXXMUSIC/utils/decorators/language.py
@@ -8,7 +8,7 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} ᴅᴀʟᴀᴍ ᴩᴇʀᴀᴡᴀᴛᴀɴ, ᴋᴜɴᴊᴜɴɢɪ <a href={ꜱᴜᴩᴩᴏʀᴛ_ᴄʜᴀᴛ}>ꜱᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ</a> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇᴛᴀʜᴜɪ ᴀʟᴀꜱᴀɴɴʏᴀ.",
                     disable_web_page_preview=True,
                 )
         try:
@@ -31,7 +31,7 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    f"{app.mention} ᴅᴀʟᴀᴍ ᴩᴇʀᴀᴡᴀᴛᴀɴ, ᴋᴜɴᴊᴜɴɢɪ ꜱᴜᴩᴩᴏʀᴛ ᴄʜᴀᴛ ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇᴛᴀʜᴜɪ ᴀʟᴀꜱᴀɴɴʏᴀ.",
                     show_alert=True,
                 )
         try:
